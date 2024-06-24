09ea70281aafa9d6f3e12689f174115eb4b3a567
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2024-06-24 17:14:41 -0300

    Validação com celebrate e criação da tabela de usuários

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 // Error Handling Middleware
 app.use(
