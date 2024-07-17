43d793d3a0e50e436fc14a4e836e5fb37ec92f09
José Carlos Rodrigues Lopes <jose.carloslopes@dataprev.gov.br>
2024-07-17 15:26:18 -0300

    Criação da entidade de OrderProducts, relacionamentos com Order e Products e repositório de Order

---
+++ src/modules/orders/typeorm/repositories/OrderRepository.ts
@@ -0,0 +1,37 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Order from '../entities/Order';
+import Customer from '@modules/customers/typeorm/entities/Customer';
+
+interface ProductDTO {
+  productId: number;
+  price: number;
+  quantity: number;
+}
+
+interface OrderDTO {
+  customer: Customer;
+  products: ProductDTO[];
+}
+
+@EntityRepository(Order)
+class OrderRepository extends Repository<Order> {
+  public async findById(id: string): Promise<Order | undefined> {
+    const order = this.findOne(id, {
+      relations: ['order_products', 'customer'],
+    });
+    return order;
+  }
+
+  public async createOrder({ customer, products }: OrderDTO): Promise<Order> {
+    const order = this.create({
+      customer,
+      orderProducts: products,
+    });
+
+    await this.save(order);
+
+    return order;
+  }
+}
+
+export default OrderRepository;
